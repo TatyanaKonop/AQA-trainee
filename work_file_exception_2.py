332b6005309ddb6264d4dd08641f9a143afc1d9a
Татьяна <korotyshtv33@gmail.com>
2023-08-29 07:54:46 +0300

    код на проверку длины перенесла в контекстный менеджер

--- work_file_exception_2.py
+++ work_file_exception_2.py
@@ -11,26 +11,31 @@ def my_context(name, method):
     file_path = os.path.join(dir_current, name) # путь к текстовому файлу
     file_object = open(file_path, method, encoding="utf-8")
     try:
-        yield file_object
+        result_symbols = file_object.read()
+        length = len(result_symbols)
+        print(f'количество символов без пробелов {length}')  # в качестве символов учитываются абзацы и пробелы
+
+        if length > 0 and length < 100:
+            middle = length // 2
+            if length % 2 == 0:  # !в случае нечетного количества символов первая часть текста больше на один символ
+                new_text = result_symbols[:(length - middle - 1):-1] + result_symbols[:middle]
+            else:
+                new_text = result_symbols[:(length - middle - 1):-1] + result_symbols[:middle + 1]
+        else:
+            raise ContainFileError
+        yield file_object, new_text
     except ContainFileError:
         print("файл пустой или слишком большой")
+        yield file_object, None
     finally:
         file_object.close()
-
-with my_context("file.txt", 'r+') as countsym:
-    result_symbols = countsym.read()
-    length = len(result_symbols)
-    print(f'количество символов без пробелов {length}') #в качестве символов учитываются абзацы и пробелы
-
-    if length > 0 and  length < 100:
-        middle = length//2
-        if length % 2 == 0: #  !в случае нечетного количества символов первая часть текста больше на один символ
-            new_text = result_symbols[:(length - middle-1):-1]+result_symbols[:middle]
-        else:
-            new_text = result_symbols[:(length - middle-1):-1] + result_symbols[:middle+1]
-        countsym.write('\n'+new_text)
+with my_context("file.txt", 'r+') as (file_object, new_text):
+    if new_text:
+        file_object.write('\n' + new_text)
     else:
-        raise ContainFileError
+        pass
+
+
 
 
 
